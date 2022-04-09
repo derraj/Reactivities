e2338243000df9beb7e77bb8f269b36ecf555e1a
jarredcm <mahinay@ualberta.ca>
2022-04-09 09:30:48 -0600

    Fixed uploading image csp

--- API/Startup.cs
+++ API/Startup.cs
@@ -51,7 +51,7 @@ namespace API
                 .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:", "https://cdn.jsdelivr.net"))
                 .FormActions(s => s.Self())
                 .FrameAncestors(s => s.Self())
-                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
+                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com","blob:"))
                 //.ScriptSources(s => s.Self().CustomSources("sha256-HIgflxNtM43xg36bBIUoPTUuo+CXZ319LsTVRtsZ/VU="))
                 .ScriptSources(s => s.Self())
             );
